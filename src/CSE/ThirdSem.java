f0e196c276b12fb1b6a1b33e1d911e78c98cb334
Praneeth <18praneeth@gmail.com>
2021-05-11 09:51:46 +0530

    Updated Program 4

---
+++ src/CSE/ThirdSem.java
@@ -0,0 +1,16 @@
+package CSE;
+
+public class ThirdSem {
+    void methodDefault(){
+        System.out.println("Default Method is Accessible");
+    }
+    private void methodPrivate(){
+        System.out.println("Private Method is Accessible");
+    }
+    protected void methodProtected(){
+        System.out.println("Protected Method is Accessible");
+    }
+    public void welcomeMessage(){
+        System.out.println("Welcome to 3rd Sem :)");
+    }
+}
--- src/prog3.java
+++ src/prog3.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 class circle
 {
     double radius;
